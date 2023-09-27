5af703e65a34e435a42c2ad037ca264951ea3e29
NiamhGillespie <2549880G@student.gla.ac.uk>
2023-09-27 21:52:01 +0100

    Created basic models for epics and user stories

--- src/Requirements_and_value_identification_tool/web_app/models.py
+++ src/Requirements_and_value_identification_tool/web_app/models.py
@@ -1,3 +1,79 @@
 from django.db import models
 
 # Create your models here.
+class Epic(models.Model):
+    epic_id = models.CharField(max_length=8) #8 digits specific only - constrain this
+    dashboard_id = models.CharField(max_length=8) #this will be a foreign key
+    title = models.CharField(max_length=128)
+
+    last_edited_by = models.CharField(max_length=128)
+    last_edited = time_created = models.DateTimeField()
+    created_by = models.CharField(max_length=128)
+    time_created = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+class Story(models.Model):
+    story_id = models.CharField(max_length=8) #8 digits specific?
+    epic_id = models.CharField(max_length=8) #maybe foreign key #each story corresponds to only one epic
+    title = models.CharField(max_length=128)
+    
+
+    user_story = models.CharField(max_length=1028) #Trefine this number at later date
+    definition_of_done = models.CharField(max_length=1028)
+    value_statement = models.CharField(max_length=1028)
+
+    #make this an actual enum
+    LOW = "LOW"
+    MEDIUM = "MEDIUM"
+    HIGH = "HIGH"
+    priority = models.CharField(max_length=6, choices = [(LOW, "low priority"), (MEDIUM, "medium priority"), (HIGH, "high priority")])
+
+    #user defined tag list?
+    #state variable - based on issue board (e.g backlog, in progress, complete <- these may be defined by user)
+    #linked/dependent stories
+
+    pairable = models.BooleanField()
+    assigned_to = models.CharField(max_length=128) #this should be list of users in future
+
+    last_edited_by = models.CharField(max_length=128)
+    last_edited = time_created = models.DateTimeField()
+    created_by = models.CharField(max_length=128)
+    time_created = models.DateTimeField()
+    
+
+    def __str__(self):
+        return self.title
+
+class Task(models.Model):
+    task_id = models.CharField(max_length=8) #8 digits specific?
+    story_id = models.CharField(max_length=8) #maybe foreign key #each task corresponds to only one story
+    title = models.CharField(max_length=128)
+
+    #look into literature surrounding 'good' task habits
+    description = models.CharField(max_length=1028)
+
+    #repeated from story class
+    #user defined tags
+    #state variable
+
+    pairable = models.BooleanField()
+    assigned_to = models.CharField(max_length=128) #this should be list of users in future
+
+    last_edited_by = models.CharField(max_length=128)
+    last_edited = time_created = models.DateTimeField()
+    created_by = models.CharField(max_length=128)
+    time_created = models.DateTimeField()
+    def __str__(self):
+        return self.title
+
+#will have multiple dashboards how to refine
+#each dashboard has multiple epics
+#each epic has only one dashboard
+#each epic has multiple stories
+#a story only has one epic
+#each story can have multiple tasks
+#each task can only have one story 
+
+#consider moving logging info into own class?
