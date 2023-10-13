511c475652b00d1340db39c077a2d61e74808c91
NiamhGillespie <2549880G@student.gla.ac.uk>
2023-10-13 15:59:14 +0100

    adding unit tests for Story model (issue #4)

--- src/Requirements_and_value_identification_tool/web_app/tests.py
+++ src/Requirements_and_value_identification_tool/web_app/tests.py
@@ -6,7 +6,6 @@ class EpicCreationTests(TestCase):
     def test_epic_id_created(self):
         """
         Checks to make sure that when an Epic is created, the correct epic_id is added.
-        Example: epic_id should be "0".
         """
         epic = Epic(
                 epic_id = '0',
@@ -23,7 +22,6 @@ class EpicCreationTests(TestCase):
     def test_dashboard_id_created(self):
         """
         Checks to make sure that when an Epic is created, the correct dashboard_id is added.
-        Example: dashboard_id should be "0".
         """
         epic = Epic(
                 epic_id = '0',
@@ -40,7 +38,6 @@ class EpicCreationTests(TestCase):
     def test_title_created(self):
         """
         Checks to make sure that when an Epic is created, the correct title is added.
-        Example: title should be "Test Epic Title".
         """
         epic = Epic(
                 epic_id = '0',
@@ -57,7 +54,6 @@ class EpicCreationTests(TestCase):
     def test_last_edited_by_field_created(self):
         """
         Checks to make sure that when an Epic is created, the correct last_edited_by is added.
-        Example: last_edited_by should be "Niamh Gillespie".
         """
         epic = Epic(
                 epic_id = '0',
@@ -74,7 +70,6 @@ class EpicCreationTests(TestCase):
     def test_last_edited_field_created(self):
         """
         Checks to make sure that when an Epic is created, the correct last_edited is added.
-        Example: last_edited should be todays date.
         """
         epic = Epic(
                 epic_id = '0',
@@ -91,7 +86,6 @@ class EpicCreationTests(TestCase):
     def test_created_by_field_created(self):
         """
         Checks to make sure that when an Epic is created, the correct created_by is added.
-        Example: created_by should be todays date.
         """
         epic = Epic(
                 epic_id = '0',
@@ -108,7 +102,6 @@ class EpicCreationTests(TestCase):
     def test_time_created_field_created(self):
         """
         Checks to make sure that when an Epic is created, the correct time_created is added.
-        Example: time_created should be todays date.
         """
         epic = Epic(
                 epic_id = '0',
@@ -121,3 +114,292 @@ class EpicCreationTests(TestCase):
             )
         epic.save()
         self.assertEqual(epic.time_created, date.today())
+
+class StoryCreationTests(TestCase):
+    def test_story_id_created(self):
+        """
+        Checks to make sure that when a Story is created, the correct story_id is added.
+        """
+        story = Story(
+                story_id = '0',
+                epic_id = '1',
+                title = 'Test Story Title',
+                user_story = 'As a developer, I want to create unit tests, So that I can continously integrate my code',
+                definition_of_done = 'Set up a unit test suite',
+                value_statement = 'ensures functional requirements of code are met',
+                priority = 'MEDIUM',
+                pairable = False,
+                assigned_to = 'Niamh Gillespie',
+                last_edited_by = 'Niamh Gillespie',
+                last_edited = date.today(),
+                created_by = 'Niamh Gillespie',
+                time_created = date.today()
+            )
+        story.save()
+        self.assertEqual(story.story_id, '0')
+
+    def test_epic_id_created(self):
+        """
+        Checks to make sure that when a Story is created, the correct epid_id is added.
+        """
+        story = Story(
+                story_id = '0',
+                epic_id = '1',
+                title = 'Test Story Title',
+                user_story = 'As a developer, I want to create unit tests, So that I can continously integrate my code',
+                definition_of_done = 'Set up a unit test suite',
+                value_statement = 'ensures functional requirements of code are met',
+                priority = 'MEDIUM',
+                pairable = False,
+                assigned_to = 'Niamh Gillespie',
+                last_edited_by = 'Niamh Gillespie',
+                last_edited = date.today(),
+                created_by = 'Niamh Gillespie',
+                time_created = date.today()
+            )
+        story.save()
+        self.assertEqual(story.epic_id, '1')
+
+    def test_title_created(self):
+        """
+        Checks to make sure that when a Story is created, the correct title is added.
+        """
+        story = Story(
+                story_id = '0',
+                epic_id = '1',
+                title = 'Test Story Title',
+                user_story = 'As a developer, I want to create unit tests, So that I can continously integrate my code',
+                definition_of_done = 'Set up a unit test suite',
+                value_statement = 'ensures functional requirements of code are met',
+                priority = 'MEDIUM',
+                pairable = False,
+                assigned_to = 'Niamh Gillespie',
+                last_edited_by = 'Niamh Gillespie',
+                last_edited = date.today(),
+                created_by = 'Niamh Gillespie',
+                time_created = date.today()
+            )
+        story.save()
+        self.assertEqual(story.title, 'Test Story Title')
+
+    def test_user_story_created(self):
+        """
+        Checks to make sure that when a Story is created, the correct user_story is added.
+        """
+        story = Story(
+                story_id = '0',
+                epic_id = '1',
+                title = 'Test Story Title',
+                user_story = 'As a developer, I want to create unit tests, So that I can continously integrate my code',
+                definition_of_done = 'Set up a unit test suite',
+                value_statement = 'ensures functional requirements of code are met',
+                priority = 'MEDIUM',
+                pairable = False,
+                assigned_to = 'Niamh Gillespie',
+                last_edited_by = 'Niamh Gillespie',
+                last_edited = date.today(),
+                created_by = 'Niamh Gillespie',
+                time_created = date.today()
+            )
+        story.save()
+        self.assertEqual(story.user_story, 'As a developer, I want to create unit tests, So that I can continously integrate my code')
+
+    def test_definition_of_done_created(self):
+        """
+        Checks to make sure that when a Story is created, the correct definition_of_done is added.
+        """
+        story = Story(
+                story_id = '0',
+                epic_id = '1',
+                title = 'Test Story Title',
+                user_story = 'As a developer, I want to create unit tests, So that I can continously integrate my code',
+                definition_of_done = 'Set up a unit test suite',
+                value_statement = 'ensures functional requirements of code are met',
+                priority = 'MEDIUM',
+                pairable = False,
+                assigned_to = 'Niamh Gillespie',
+                last_edited_by = 'Niamh Gillespie',
+                last_edited = date.today(),
+                created_by = 'Niamh Gillespie',
+                time_created = date.today()
+            )
+        story.save()
+        self.assertEqual(story.definition_of_done, 'Set up a unit test suite')
+
+    def test_value_statement_created(self):
+        """
+        Checks to make sure that when a Story is created, the correct value_statement is added.
+        """
+        story = Story(
+                story_id = '0',
+                epic_id = '1',
+                title = 'Test Story Title',
+                user_story = 'As a developer, I want to create unit tests, So that I can continously integrate my code',
+                definition_of_done = 'Set up a unit test suite',
+                value_statement = 'ensures functional requirements of code are met',
+                priority = 'MEDIUM',
+                pairable = False,
+                assigned_to = 'Niamh Gillespie',
+                last_edited_by = 'Niamh Gillespie',
+                last_edited = date.today(),
+                created_by = 'Niamh Gillespie',
+                time_created = date.today()
+            )
+        story.save()
+        self.assertEqual(story.value_statement, 'ensures functional requirements of code are met')
+    
+    def test_priority_created(self):
+        """
+        Checks to make sure that when a Story is created, the correct priority is added.
+        """
+        story = Story(
+                story_id = '0',
+                epic_id = '1',
+                title = 'Test Story Title',
+                user_story = 'As a developer, I want to create unit tests, So that I can continously integrate my code',
+                definition_of_done = 'Set up a unit test suite',
+                value_statement = 'ensures functional requirements of code are met',
+                priority = 'MEDIUM',
+                pairable = False,
+                assigned_to = 'Niamh Gillespie',
+                last_edited_by = 'Niamh Gillespie',
+                last_edited = date.today(),
+                created_by = 'Niamh Gillespie',
+                time_created = date.today()
+            )
+        story.save()
+        self.assertEqual(story.priority, 'MEDIUM')
+
+
+    def test_pairable_created(self):
+        """
+        Checks to make sure that when a Story is created, the correct pairable boolean is added.
+        """
+        story = Story(
+                story_id = '0',
+                epic_id = '1',
+                title = 'Test Story Title',
+                user_story = 'As a developer, I want to create unit tests, So that I can continously integrate my code',
+                definition_of_done = 'Set up a unit test suite',
+                value_statement = 'ensures functional requirements of code are met',
+                priority = 'MEDIUM',
+                pairable = False,
+                assigned_to = 'Niamh Gillespie',
+                last_edited_by = 'Niamh Gillespie',
+                last_edited = date.today(),
+                created_by = 'Niamh Gillespie',
+                time_created = date.today()
+            )
+        story.save()
+        self.assertEqual(story.pairable, False)
+    
+    def test_assigned_to_created(self):
+        """
+        Checks to make sure that when a Story is created, the correct assigned_to is added.
+        """
+        story = Story(
+                story_id = '0',
+                epic_id = '1',
+                title = 'Test Story Title',
+                user_story = 'As a developer, I want to create unit tests, So that I can continously integrate my code',
+                definition_of_done = 'Set up a unit test suite',
+                value_statement = 'ensures functional requirements of code are met',
+                priority = 'MEDIUM',
+                pairable = False,
+                assigned_to = 'Niamh Gillespie',
+                last_edited_by = 'Niamh Gillespie',
+                last_edited = date.today(),
+                created_by = 'Niamh Gillespie',
+                time_created = date.today()
+            )
+        story.save()
+        self.assertEqual(story.assigned_to, 'Niamh Gillespie')
+
+    def test_last_edited_by_created(self):
+        """
+        Checks to make sure that when a Story is created, the correct assigned_to is added.
+        """
+        story = Story(
+                story_id = '0',
+                epic_id = '1',
+                title = 'Test Story Title',
+                user_story = 'As a developer, I want to create unit tests, So that I can continously integrate my code',
+                definition_of_done = 'Set up a unit test suite',
+                value_statement = 'ensures functional requirements of code are met',
+                priority = 'MEDIUM',
+                pairable = False,
+                assigned_to = 'Niamh Gillespie',
+                last_edited_by = 'Niamh Gillespie',
+                last_edited = date.today(),
+                created_by = 'Niamh Gillespie',
+                time_created = date.today()
+            )
+        story.save()
+        self.assertEqual(story.last_edited_by, 'Niamh Gillespie')
+
+    def test_last_edited_created(self):
+        """
+        Checks to make sure that when a Story is created, the correct last_edited is added.
+        """
+        story = Story(
+                story_id = '0',
+                epic_id = '1',
+                title = 'Test Story Title',
+                user_story = 'As a developer, I want to create unit tests, So that I can continously integrate my code',
+                definition_of_done = 'Set up a unit test suite',
+                value_statement = 'ensures functional requirements of code are met',
+                priority = 'MEDIUM',
+                pairable = False,
+                assigned_to = 'Niamh Gillespie',
+                last_edited_by = 'Niamh Gillespie',
+                last_edited = date.today(),
+                created_by = 'Niamh Gillespie',
+                time_created = date.today()
+            )
+        story.save()
+        self.assertEqual(story.last_edited, date.today())
+
+
+    def test_created_by_created(self):
+        """
+        Checks to make sure that when a Story is created, the correct created_by is added.
+        """
+        story = Story(
+                story_id = '0',
+                epic_id = '1',
+                title = 'Test Story Title',
+                user_story = 'As a developer, I want to create unit tests, So that I can continously integrate my code',
+                definition_of_done = 'Set up a unit test suite',
+                value_statement = 'ensures functional requirements of code are met',
+                priority = 'MEDIUM',
+                pairable = False,
+                assigned_to = 'Niamh Gillespie',
+                last_edited_by = 'Niamh Gillespie',
+                last_edited = date.today(),
+                created_by = 'Niamh Gillespie',
+                time_created = date.today()
+            )
+        story.save()
+        self.assertEqual(story.created_by, 'Niamh Gillespie')
+
+    def test_time_created_created(self):
+        """
+        Checks to make sure that when a Story is created, the correct time_created is added.
+        """
+        story = Story(
+                story_id = '0',
+                epic_id = '1',
+                title = 'Test Story Title',
+                user_story = 'As a developer, I want to create unit tests, So that I can continously integrate my code',
+                definition_of_done = 'Set up a unit test suite',
+                value_statement = 'ensures functional requirements of code are met',
+                priority = 'MEDIUM',
+                pairable = False,
+                assigned_to = 'Niamh Gillespie',
+                last_edited_by = 'Niamh Gillespie',
+                last_edited = date.today(),
+                created_by = 'Niamh Gillespie',
+                time_created = date.today()
+            )
+        story.save()
+        self.assertEqual(story.time_created, date.today())
