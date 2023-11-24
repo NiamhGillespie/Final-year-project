5e461de3c729afd43fca1d86f78f2708012f53fb
NiamhGillespie <2549880G@student.gla.ac.uk>
2023-11-24 00:19:15 +0000

    Displaying the relevant stories in the tracking columns (issue #16)

--- src/Requirements_and_value_identification_tool/react-app/src/components/tracking_dashboard.js
+++ src/Requirements_and_value_identification_tool/react-app/src/components/tracking_dashboard.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import { API_URL_DASHBOARD_TRACKING_COLUMNS, API_URL } from "../constants";
 import AddColumnModal from './add_tracking_column_modal';
 import EditColumnModal from './edit_tracking_column_modal';
+import { DragDropContext, Droppable, Draggable   } from 'react-beautiful-dnd';
 
 
 export class TrackingDashboard extends Component {
@@ -23,7 +24,7 @@ export class TrackingDashboard extends Component {
         await axios.get(API_URL_DASHBOARD_TRACKING_COLUMNS).then(response => this.setState({ columns: response.data }));
     };
 
-    async getUndefinedStories() {
+    async getNonCompletedStories() {
         await axios.get(API_URL).then(response => this.setState({ non_completed_stories: response.data[1].filter(story => story.state != 'done') }));
     }
 
@@ -33,7 +34,7 @@ export class TrackingDashboard extends Component {
    
     resetState= () => {
         this.getColumns();
-        this.getUndefinedStories();
+        this.getNonCompletedStories();
         this.getEpics();
         
     };
@@ -52,6 +53,8 @@ export class TrackingDashboard extends Component {
                         epics={this.state.epics}/>
                         <p> { columns[i].stories.length } </p>
                         <hr className='pt-0 mt-0'></hr>
+
+                        {this.displayStories(columns[i].stories)}
                     </div>
                 )
             }
@@ -63,6 +66,40 @@ export class TrackingDashboard extends Component {
 
         return return_list;
     }
+
+    displayStories(story_ids) {      
+        const getDraggingStyle = isDraggingOver => ({
+            background: isDraggingOver ? "#"  + "20" : "",
+            paddingTop: 2,
+            paddingBottom: 2,
+            borderRadius: 10,
+          });
+
+        var returnList = [];
+        var stories = [];
+        if (story_ids != undefined && story_ids != []) {
+            for (var i = 0; i < story_ids.length; i++) {
+                stories.push(this.state.non_completed_stories.filter(story => story.id == story_ids[i])[0])
+            }
+
+            if (stories[0] != undefined) {
+                console.log("STORIES", stories[0][0]);
+                for (var i = 0; i < stories.length; i++) {
+                    returnList.push (
+                        <div style={{border: '2px solid ' + 'red'}} className="story-box">
+                            <p className='story-title'> {stories[i].title} </p>
+                            <p style={{background: 'red'}} className='story-profile-photo'> icon </p>
+                            <p className='story-priority'> {(stories[i].priority)} </p>
+                        </div>
+                    )
+                }
+
+        }
+
+        return returnList;
+    }
+}
+
     render() {
         return (
             <>
